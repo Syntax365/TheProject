ec08bcc6ec326edf3120bdc26166d9749ad8cb7f
Syntax365 <synxtax365@gmail.com>
2021-10-10 12:36:11 -0400

    Add GTM

--- server.js
+++ server.js
@@ -24,15 +24,30 @@ app.get("*", (req, res) => {
 
   const helmet = Helmet.renderStatic();
 
+  const gtmHead = `<!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KGBZ5X2');</script>
+  <!-- End Google Tag Manager -->`;
+
+  const gtmBody = `<!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGBZ5X2"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->`;
+
   const html = `
     <html>
         <head>
+          ${gtmHead}
           ${helmet.meta.toString()}
           ${helmet.title.toString()}
         </head>
         <body>
             <div id="root">${content}</div>
         </body>
+          ${gtmBody}
         <script src="client_bundle.js"></script>
     </html>
   `;
