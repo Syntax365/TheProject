e714359959d1b3f6fbecf46200fd2f50476ff2c9
Syntax365 <synxtax365@gmail.com>
2021-10-17 21:31:53 -0400

    Fix SEO lang attr

--- renderer.js
+++ renderer.js
@@ -24,7 +24,7 @@ export default function renderer(req, res) {
   const helmet = Helmet.renderStatic();
 
   const html = `
-    <html>
+    <html lang="en">
       <head>
             ${gtmHead}
             ${helmet.meta.toString()}
